289fa0912e68dbeccbd92a20c9b7ef39d0d0734e
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-11-17 10:28:09 -0500

    configured app for login with fb

--- app/src/main/java/io/github/vanessa85/fbandroidtest/MainActivity.java
+++ app/src/main/java/io/github/vanessa85/fbandroidtest/MainActivity.java
@@ -1,13 +1,95 @@
 package io.github.vanessa85.fbandroidtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String userId = loginResult.getAccessToken().getUserId();
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        displayUserInfo(object);
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email, id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    public void displayUserInfo(JSONObject object) {
+        String firstName, lastName, email, id;
+
+        firstName = "";
+        lastName = "";
+        email = "";
+        id = "";
+
+        try {
+            firstName = object.getString("first_name");
+            lastName = object.getString("last_name");
+            email = object.getString("email");
+            id = object.getString("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        TextView tvFullName, tvEmail, tvId;
+
+        tvFullName = findViewById(R.id.tv_fullname);
+        tvEmail = findViewById(R.id.tv_email);
+        tvId = findViewById(R.id.tv_id);
+
+        tvFullName.setText(firstName + " " + lastName);
+        tvEmail.setText(email);
+        tvId.setText(id);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
